2710644a61982b6b4d5cb7441ae645be1f2c5f0d
JoaoQueroga <joaoqueroga@gmail.com>
2021-12-23 09:25:17 -0400

    front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [fotos, setFotos] = useState([]);
+  const URL = "http://localhost:8080/"
+
+  function handleUploadFile(event){
+    const data = new FormData();
+
+    data.append('file', event.target.files[0]);
+    data.append('name', 'some value user types');
+    data.append('description', 'some value user types');
+    
+    axios.post('http://localhost:8080/foto', data ).then((response) => {
+      console.log("upload de foto realizado");
+    }).then(()=>{
+      axios.get('http://localhost:8080/baixar-fotos').then((response) => {
+        setFotos(response.data);
+      });
+    });
+  }
+
+  useEffect(()=>{
+    axios.get('http://localhost:8080/baixar-fotos').then((response) => {
+      setFotos(response.data);
+    });
+  },[])
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>upload de imagem</h1>
+      <input type="file" onChange={handleUploadFile}/>
+      {
+        fotos.map((foto)=>{
+          return(
+            <div key={foto.id}>
+              <br/>
+              <img  src={URL+foto.url} alt='foto' width={600} height={400}/>
+              <br/>
+            </div>
+          )
+        })
+      }
+      
+    </div> 
   );
 }
 
